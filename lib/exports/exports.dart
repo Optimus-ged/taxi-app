b8251c800eea63a0e99f148941a07211da766a36
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-02-26 15:06:16 +0200

    Adding google maps dependancies. next step get an id for google console acount

--- lib/exports/exports.dart
+++ lib/exports/exports.dart
@@ -1,5 +1,7 @@
 // Flutter dependancies
 export 'package:flutter/material.dart';
+export 'dart:async';
+export 'package:google_maps_flutter/google_maps_flutter.dart';
 
 // Flutter Pages
 export 'package:taxi_app/pages/home_page.dart';
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,35 @@
 import 'package:taxi_app/exports/exports.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Completer<GoogleMapController> _googleMapController = Completer();
+  GoogleMapController newGoogleMapController;
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(body: Container(),),
+      child: Scaffold(
+        body: Stack(
+          children: [
+            GoogleMap(
+              mapType: MapType.normal,
+              myLocationButtonEnabled: true,
+              initialCameraPosition: _kGooglePlex,
+              onMapCreated: (controller) {
+                _googleMapController.complete(controller);
+                newGoogleMapController = controller;
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
