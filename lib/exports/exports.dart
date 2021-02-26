a116d2648d9bf12e4d28369891af8d27c93ab305
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-02-26 13:20:12 +0200

    Adding packages and improvement of projetct structure

---
+++ lib/exports/exports.dart
@@ -0,0 +1,5 @@
+// Flutter dependancies
+export 'package:flutter/material.dart';
+
+// Flutter Pages
+export 'package:taxi_app/pages/home_page.dart';
---
+++ lib/pages/home_page.dart
@@ -0,0 +1,10 @@
+import 'package:taxi_app/exports/exports.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(body: Container(),),
+    );
+  }
+}
