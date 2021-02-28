b65fbda88bc82087d8d01171440abc19f4e58bf3
Optimus Ged <gedeonyalakuhanda@gmail.com>
2021-02-28 22:25:20 +0200

    Initialisation du login page

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,16 @@
+import 'package:taxi_app/exports/exports.dart';
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Column(
+            children: [],
+          ),
+        ),
+      ),
+    );
+  }
+}
